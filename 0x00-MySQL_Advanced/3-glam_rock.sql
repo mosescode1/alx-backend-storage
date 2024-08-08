8aa0c33910287c82f13efcfaeac01bea7794e312
mosescode1 <emosescode@gmail.com>
2024-08-08 15:09:43 +0100

    storage

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Lists all bands with glam rock main style, ranked by longetivity
+SELECT band_name, (IFNULL(split, '2022')) - formed 
+AS lifespan FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style,'')) > 0
+ORDER BY lifespan DESC;
