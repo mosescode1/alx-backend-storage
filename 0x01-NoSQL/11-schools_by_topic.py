54daf528a144fdf570ee31b690fe0d53c1c2987a
mosescode1 <emosescode@gmail.com>
2024-08-13 09:28:52 +0100

    done

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: document
+    """
+
+    if mongo_collection is None:
+        return None
+
+    return mongo_collection.find({"topics": topic})
