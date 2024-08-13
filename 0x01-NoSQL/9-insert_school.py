7f5ec48c1df76e03db87b1494cb22531793d1796
mosescode1 <emosescode@gmail.com>
2024-08-13 09:13:18 +0100

    done

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Module to insert to collection"""
 
 
 def insert_school(mongo_collection, **kwargs):
     """Function insering to collection"""
 
-    value = mongo_collection.insert_one(kwargs);
+    value = mongo_collection.insert_one(kwargs)
     return value.inserted_id
