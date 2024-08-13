a9c39b3c9dd0ece357fb5abf0e93f7972b932cb6
mosescode1 <emosescode@gmail.com>
2024-08-13 12:09:22 +0100

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for log stats"""
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+
+# db = client["logs"]
+# col = db["nginx"]
+db = client.logs
+col = db.nginx
+
+str = "{} logs".format(col.count_documents({}))
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print(str)
+print("Methods:")
+
+for val in method:
+    count = col.count_documents({'method': val})
+    print("\tmethod {}: {}".format(val, count))
+
+status_count = col.count_documents({'method': 'GET', "path": "/status"})
+print("{} status check".format(status_count))
