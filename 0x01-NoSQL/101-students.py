5fbe79b4df8f07ad965113c1acc081bd7f1c30ee
mosescode1 <emosescode@gmail.com>
2024-08-13 12:48:08 +0100

    done

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,13 @@
+""""sumary_line"""
+
+
+def top_students(mongo_collection):
+    """Returns Top Student"""
+    return mongo_collection.aggregate([
+        {'$project': {
+            'name': '$name',
+            'averageScore': {'$avg': '$topics.score'}
+        }},
+
+        {'$sort': {'averageScore': -1}}
+    ])
